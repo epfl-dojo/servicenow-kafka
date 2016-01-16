6c42ed1710ac9a1567e904e55a91c0fe5a63f8b6
Dominique Quatravaux <dominique@quatravaux.org>
2016-01-16 18:15:13 +0100

    Dojo du 15 janvier 2016
    * Requête via l'API ServiceNow, en utilisant un bocal de cookies sauvegardé depuis
      le navigateur (extension Export Cookies pour Firefox)
    * Insertion dans un Kafka Dockerisé (docker-compose)

---
+++ README.md
@@ -0,0 +1,19 @@
+# README
+
+# Objectifs
+
+Process #1
+
+1. connect to Service Now
+2. pull ticket
+3. feed them to kafka
+
+Process #2
+
+1. tail tickets from kafka: 
+  docker exec -it kafka_kafka_1 /bin/bash
+  cd /opt/kafka_2.10-0.8.2.0/bin
+  ./kafka-console-consumer.sh --zookeeper zookeeper_1 --topic servicenow-tickets
+
+
+https://it.epfl.ch/backoffice/api/now/v1/table/incident?sysparm_limit=10&sysparm_query=active=true^ORDERBYnumber^ORDERBYDESCcategory
---
+++ test-kafka-producer.js
@@ -0,0 +1,59 @@
+/**
+ * New module.
+ */
+
+var kafka = require('kafka-node'),
+    Producer = kafka.Producer,
+    serviceNowURLBase = 'https://it.epfl.ch/backoffice/api/now/v1/table/incident?sysparm_limit=20';
+
+
+
+function requestWithCookies(url, done) {
+    var request = require('request');
+    var cookie = require('dunkatron-cookies');
+
+    cookie.parse('/Users/dom/Dev/dojo/kafka/cookies.txt', function (err, jsonCookieObj) {
+        request
+            .get({
+                url:url,
+                jar:true,
+                encoding:null,
+                headers:{
+                    Cookie:jsonCookieObj.getCookieString(url)
+                }
+            }, done)
+    });
+}
+
+function getSomeTickets(query, done) {
+    requestWithCookies(serviceNowURLBase + "&" + query, function (err, result, body) {
+        if (err) {
+            done(err, null);
+        } else {
+            done(null, JSON.parse(body));
+        }
+    });
+}
+
+getSomeTickets('sysparm_query=active=true', function (err, result) {
+    console.log(JSON.stringify(result));
+
+    var client = new kafka.Client("192.168.99.100:2181"), // connectionString: Zookeeper connection string, default localhost:2181/
+        producer = new Producer(client),
+        payloads = result.result.map(function (ticket) {
+            return { topic: 'servicenow-tickets', messages: ticket.number, partition: 0 }
+        });
+
+    producer.on('ready', function () {
+        console.log("Producer run ready");
+        producer.send(payloads, function (err, data) {
+            console.log(data + "SEND ERROR: " + err);
+        });
+    });
+
+    producer.on('error', function (err) {console.log("KAFKA ERROR: " + err)});
+
+
+
+});
+
