086197eb7b313a29cd84dbc14a066db16f69fd2a
Dominique Quatravaux <dominique@quatravaux.org>
2016-01-22 14:45:13 +0100

    Add query for tickets changed after date X
    Plus some ahead-planning (done out of the dojo)

--- test-kafka-producer.js
+++ test-kafka-producer.js
@@ -3,9 +3,10 @@
  */
 
 var kafka = require('kafka-node'),
+    Consumer = kafka.Consumer,
     Producer = kafka.Producer,
-    serviceNowURLBase = 'https://it.epfl.ch/backoffice/api/now/v1/table/incident?sysparm_limit=20';
-
+    serviceNowURLBase = 'https://it.epfl.ch/backoffice/api/now/v1/table/incident?sysparm_limit=20',
+    dateFormat = require('dateformat');
 
 
 function requestWithCookies(url, done) {
@@ -35,33 +36,38 @@ function getSomeTickets(query, done) {
     });
 }
 
-//getSomeTickets('sysparm_query=sys_updated_at&gt;')
-// https://it.epfl.ch/backoffice/incident_list.do?sysparm_query=sys_updated_on>javascript:gs.dateGenerate('2016-01-21','23:59:59')&sysparm_first_row=1&sysparm_view=
-//return;
+function pumpMoreTickets(lastDate) {
+    var day = dateFormat(lastDate, "yyyy-mm-dd");
+    var hour = dateFormat(lastDate, "HH:MM:ss");
+    getSomeTickets("sysparm_query=sys_updated_on>javascript:gs.dateGenerate('" + day + "','" + hour + "')",
+        function (err, result) {
+        console.log(JSON.stringify(result));
 
-getSomeTickets("sysparm_query=sys_updated_on>javascript:gs.dateGenerate('2016-01-21','23:59:59')", function (err, result) {
-    console.log(JSON.stringify(result));
+        var client = new kafka.Client("192.168.99.100:2181"), // connectionString: Zookeeper connection string, default localhost:2181/
+            producer = new Producer(client),
+            messages = result.result.map(function (ticket) {
+                return JSON.stringify({
+                    id: ticket.number,
+                    opened_at: ticket.opened_at,
+                    updated_at: ticket.sys_updated_on
+                });
+            });
 
-    var client = new kafka.Client("192.168.99.100:2181"), // connectionString: Zookeeper connection string, default localhost:2181/
-        producer = new Producer(client),
-        messages = result.result.map(function (ticket) {
-            return JSON.stringify({
-                id: ticket.number,
-                opened_at: ticket.opened_at,
-                updated_at: ticket.sys_updated_on
+        producer.on('ready', function () {
+            console.log("Producer run ready");
+            producer.send([{ topic: 'servicenow-tickets', messages: messages, partition: 0 }], function (err, data) {
+                console.log(data + "SEND ERROR: " + err);
             });
         });
 
-    producer.on('ready', function () {
-        console.log("Producer run ready");
-        producer.send([{ topic: 'servicenow-tickets', messages: messages, partition: 0 }], function (err, data) {
-            console.log(data + "SEND ERROR: " + err);
-        });
+        producer.on('error', function (err) {console.log("KAFKA ERROR: " + err)});
+
+
+
     });
+}
 
-    producer.on('error', function (err) {console.log("KAFKA ERROR: " + err)});
-
-
-
-});
-
+var lastSeenChange = getLatestTicket(new Consumer(client)).updated_at;
+while(true) {
+    pumpMoreTickets(lastSeenChange);
+}
