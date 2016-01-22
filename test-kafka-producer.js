c0a8ba00a555a172eace322c30baf1b95670fc49
Dominique Quatravaux <dominique@quatravaux.org>
2016-01-22 13:43:01 +0100

    Query tickets changed after a certain date

--- test-kafka-producer.js
+++ test-kafka-producer.js
@@ -35,13 +35,21 @@ function getSomeTickets(query, done) {
     });
 }
 
-getSomeTickets('sysparm_query=active=true', function (err, result) {
+//getSomeTickets('sysparm_query=sys_updated_at&gt;')
+// https://it.epfl.ch/backoffice/incident_list.do?sysparm_query=sys_updated_on>javascript:gs.dateGenerate('2016-01-21','23:59:59')&sysparm_first_row=1&sysparm_view=
+//return;
+
+getSomeTickets("sysparm_query=sys_updated_on%3Ejavascript:gs.dateGenerate%28%272016-01-21%27,%2723:59:59%27%29", function (err, result) {
     console.log(JSON.stringify(result));
 
     var client = new kafka.Client("192.168.99.100:2181"), // connectionString: Zookeeper connection string, default localhost:2181/
         producer = new Producer(client),
         messages = result.result.map(function (ticket) {
-            return JSON.stringify({id: ticket.number, opened_at: ticket.opened_at});
+            return JSON.stringify({
+                id: ticket.number,
+                opened_at: ticket.opened_at,
+                updated_at: ticket.sys_updated_on
+            });
         });
 
     producer.on('ready', function () {
